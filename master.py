e3bb17e168d13c19d04c947b14783f79b79b02fd
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-09-16 14:18:39 +0530

    Update master.py

--- master.py
+++ master.py
@@ -4,9 +4,7 @@ import modules
 ch=int(input("Press 1 to add and 2 to remove"))
 if ch == 1:
     cm = menus.ContextMenu('Easyry', type='FILES')
-    cm2 = menus.ContextMenu('Some Function')
     cm.add_items([
-                cm2,
                 menus.ContextCommand('Func1', python=modules.func1)
             ])
     cm.compile()
