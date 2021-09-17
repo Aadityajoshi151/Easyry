1e8c11d5ea98a94bd374e862ec87bbf67a8b676a
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-09-17 10:39:49 +0530

    added icon

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ch=int(input("Press 1 to install and 2 to uninstall\n"))
 if ch == 1:
     cm = menus.ContextMenu('Easyry', type='FILES')
     cm.add_items([
-                menus.ContextCommand('Move To Folder', python=modules.moveToFolder)
+                menus.ContextCommand('📁 Move To Folder', python=modules.moveToFolder)
             ])
     cm.compile()
     print("Easyry Installed!")
