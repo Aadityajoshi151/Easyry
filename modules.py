072f14e69a8ae80f8a34c520b0f3410114cf1760
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-10-01 20:50:03 +0530

    Timestamp printing

--- modules.py
+++ modules.py
@@ -4,6 +4,8 @@ import string
 import random
 import pathlib
 import pyperclip as pc
+from datetime import datetime
+
 
 def moveToFolder(path,params):
     path=path[0]
@@ -22,6 +24,13 @@ def renameRandom(path,params):
     randomstr = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
     extension = pathlib.Path(path).suffix
     os.rename(path,path[:path.rfind("\\")]+"\\"+randomstr+extension)
+
+def renameTimestamp(path,params):
+    path=path[0]
+    dateTimeObj = datetime.now()
+    print(f"{dateTimeObj.day}-{dateTimeObj.month}-{dateTimeObj.year} {dateTimeObj.hour}:{dateTimeObj.minute}:{dateTimeObj.second}")
+    input("Press Any Key To Continue...")
+
 
 def extractName(path): return path[path.rfind("\\")+1:]
 
