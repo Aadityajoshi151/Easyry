52614c44c1799f0aad10cdb861d3513dae468a49
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-10-01 21:04:07 +0530

    rename with timestamp functionality added
    A general rename function is also added

--- modules.py
+++ modules.py
@@ -22,17 +22,18 @@ def copyName(path,params): pc.copy(extractName(path[0]))
 def renameRandom(path,params):
     path = path[0]
     randomstr = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
-    extension = pathlib.Path(path).suffix
-    os.rename(path,path[:path.rfind("\\")]+"\\"+randomstr+extension)
+    rename(path,randomstr)
 
 def renameTimestamp(path,params):
     path=path[0]
     dateTimeObj = datetime.now()
-    print(f"{dateTimeObj.day}-{dateTimeObj.month}-{dateTimeObj.year} {dateTimeObj.hour}:{dateTimeObj.minute}:{dateTimeObj.second}")
-    input("Press Any Key To Continue...")
-
+    timestamp = str(dateTimeObj.day)+"-"+str(dateTimeObj.month)+"-"+str(dateTimeObj.year)+" "+str(dateTimeObj.hour)+"-"+str(dateTimeObj.minute)+"-"+str(dateTimeObj.second)
+    rename(path,timestamp)
 
 def extractName(path): return path[path.rfind("\\")+1:]
 
 def removeExtension(name): return name[:name.rfind(".")]
 
+def rename(path,newname):
+    extension = pathlib.Path(path).suffix
+    os.rename(path,path[:path.rfind("\\")]+"\\"+newname+extension)
